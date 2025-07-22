3742f8ba79456397d746f6433ae5ea7f9a163830
jay.pacamarra <jay.pacamarra@sanctuary.ai>
2025-07-22 12:09:47 -0700

    add multimonitor polybar support

--- polybar/.config/polybar/forest/launch.sh
+++ polybar/.config/polybar/forest/launch.sh
@@ -10,5 +10,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c "$DIR"/config.ini --reload main &
+  done
+else
+  polybar  -c "$DIR"/config.ini --reload main &
+fi

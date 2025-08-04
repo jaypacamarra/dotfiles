74ddb04aeb66722e0c32a096e6b29f00b4bd6c02
jaypacamarra <sjaypacamarra@gmail.com>
2025-08-03 20:42:36 -0700

    add oh my zsh plugins similar to zinit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,6 +18,11 @@ sudo systemctl enable lightdm
 # Install TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Install ohmyzsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+
 # reboot sudo reboot
 echo "Setup complete, reboot system with:"
 echo "sudo reboot"
